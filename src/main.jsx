2336833132a795f62327dfcb43258effe8def471
Connor Hill <conhill@optonline.net>
2025-06-02 16:10:43 -0400

    updating endpoint

--- src/main.jsx
+++ src/main.jsx
@@ -132,7 +132,7 @@ export default function BangerUploader() {
       console.log('FormData entries:', [...formData.entries()]);
       
       // Call your Flask API
-      const response = await fetch('http://localhost:5000/predict', {
+      const response = await fetch('https://music-flask-api.onrender.com/predict', {
         method: 'POST',
         body: formData
       });
